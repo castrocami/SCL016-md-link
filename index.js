b98a06147d40c8669430515e3af096e8d7957c53
Camila Castro <ccastromn@gmail.com>
2021-05-05 16:43:54 -0300

    Feat: Function that reads the total lines of the readme

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-module.exports = () => {
-  // ...
+// module.exports = () => {}
+// Node libraries 
+const fs = require("fs");
+const readline = require("readline");
+
+const mdLinks = (path, { validate }) => {};
+
+/**
+ * https://stackoverflow.com/questions/13104411/how-to-specify-resolution-and-rejection-type-of-the-promise-in-jsdoc
+ * @param {String} path route to file for link extraction 
+ * @returns {Promise<[{line: Number, link: String}]>}
+ */
+const getLinksFromFile = (path) => {
+  // https://nodejs.org/api/readline.html#readline_example_read_file_stream_line_by_line
+  return new Promise((resolve, reject) => {
+    //Read file
+    const rl = readline.createInterface({
+      input: fs.createReadStream(path),
+      crlfDelay: Infinity,
+    });
+    // Regex emails
+    const re = /\b(https?:\/\/\S*\b)/g;
+    let lineNumber = 0;
+    rl.on("line", (line) => {
+      console.log(`${++lineNumber}: ${line}`);
+      const found = line.match(re);
+      console.log(found);
+      if (found != null) {
+        console.log("largo: " + found.length);
+      }
+    });
+    // End of file
+    rl.on("close", () => {
+      resolve(lineNumber);
+    });
+  });
 };
+// Consumo de promesa 
+getLinksFromFile("readmeTest.md").then((numberOfLines) => {
+  console.log(`Hay ${numberOfLines} lineas en este archivo`);
+});
+
---
+++ readmeTest.md
@@ -0,0 +1,11 @@
+# Demo links
+
+[Este es un link a google](http://google.com)
+
+[Este es un link malo](http://goo234234gle.com)
+
+https://www.google.com/search?q=perritos&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi7hKTJo6TwAhXzlZUCHR_uDuMQ_AUoAXoECAEQAw&biw=1325&bih=668
+
+https://www.google.com/sea2342344=lnms&tbm=isch&ssdf&ved=2ahUKEwsdfo6TwAhXzlZUCHR_uDuMQ_AUoAXoECAEQAw&biw=1325&bih=668
+
+[Link 1 ](http://google.com) [Link 1 ](http://facebook.com/asdasda)
