fcf51ed234ac77d97429687e12b7c2798ebe9160
Camila Castro <ccastromn@gmail.com>
2021-05-12 11:37:19 -0400

    Feat: Get status url + mdLinks function

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // Node libraries 
 const fs = require("fs");
 const readline = require("readline");
-
-const mdLinks = (path, { validate }) => {};
+const http = require('http');
+const { URL } = require('url');
 
 /**
  * https://stackoverflow.com/questions/13104411/how-to-specify-resolution-and-rejection-type-of-the-promise-in-jsdoc
@@ -18,21 +18,26 @@ const getLinksFromFile = (path) => {
       input: fs.createReadStream(path),
       crlfDelay: Infinity,
     });
-    // Regex emails
-    const re = /\b(https?:\/\/\S*\b)/g;
+    // Regex emails https://regexr.com/
+    const re = /\[([^\]]*)\]\(([^\)]+)\)/g;
     let lineNumber = 0;
-    const resultLinksAndLines = [];    
+    const resultLinksAndLines = [];
     rl.on("line", (line) => {
       lineNumber++;
-      const foundLinks = line.match(re);
-      if (foundLinks != null){
-        foundLinks.forEach((link)=>{
-          const linkObject = {
-            line : lineNumber,
-            link : link,
-          }
-          resultLinksAndLines.push(linkObject);
-        })
+      const foundLinks = line.matchAll(re);
+      // console.log(`linea ${lineNumber}: ${line}`);
+      for (const match of foundLinks) {
+        // console.log(`match total: ${match[0]}` );
+        // console.log(`text: ${match[1]}`);
+        // console.log(`link: ${match[2]}`);    
+        const linkObject = {
+          line: lineNumber,
+          file: path,
+          text: match[1],
+          link: match[2],
+
+        }
+        resultLinksAndLines.push(linkObject);
       }
     });
     // End of file
@@ -42,7 +47,77 @@ const getLinksFromFile = (path) => {
   });
 };
 
-getLinksFromFile("readmeTest.md").then((linksArray) => {
+/*getLinksFromFile("readmeTest.md").then((linksArray) => {
+  console.log(linksArray);
+});*/
+
+/**
+ * 
+ * @param {String} url url to validate
+ * @returns {{ok: String, status: Number}} 
+ */
+const getStatusForURl = (link) => {
+  return new Promise((resolve, reject) => {
+    const result = {};
+    try {
+      const url = new URL(link);
+      const options = {
+        host: url.host,
+        path: url.pathname,
+        method: 'GET',
+      };
+      const req = http.request(options, (res) => {
+        result.status = res.statusCode;
+        result.ok = 'ok';
+        resolve(result);
+      });
+      req.on('error', (error) => {
+        result.ok = `fail: ${error.message}`;
+        resolve(result);
+      });
+      req.end();
+    } catch (error) {
+      result.ok = `fail: ${error}`;
+      resolve(result);
+    }
+  });
+}
+/**
+ * 
+ * @param {String} path 
+ * @param {{validate: Boolean}} options 
+ */
+const mdLinks = (path, { validate }) => {
+  if (validate) {
+    //crear promesa que llame a getlinks from file... obtener el resultado y por cada item llamar a funcion que valide el link y cuando
+    //todo eso haya terminado resuelvo la promesa
+    return new Promise((resolve, reject) => {
+      getLinksFromFile(path).then((arrayObjects) => {
+        const promisesArray = arrayObjects.map((obj) => {
+          return getStatusForURl(obj.link);
+        })
+        //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+        Promise.all(promisesArray).then((resolvedPromisesArray) => {
+          for (let i = 0; i < arrayObjects.length; i++) {
+            // https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+            Object.assign(arrayObjects[i], resolvedPromisesArray[i]);
+          }
+          resolve(arrayObjects);
+        })
+
+      });
+
+    })
+  } else {
+    return getLinksFromFile(path);
+  }
+};
+
+mdLinks("readmeTest.md", { validate: true }).then((linksArray) => {
   console.log(linksArray);
 });
+
+// getStatusForURl("https://aula.cdichile.org/mod/page/view.php?id=749&forceview=1").then(result => {
+//   console.log(result);
+// })
 
