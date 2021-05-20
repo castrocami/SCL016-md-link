659b73a3b13e14e6b43e62fe648d64f76a970779
Camila Castro <ccastromn@gmail.com>
2021-05-20 19:53:10 -0400

    Feat : Validate and default options

--- cli.js
+++ cli.js
@@ -1,7 +1,36 @@
 #!/usr/bin/env node
+const { program } = require('commander');
 const { mdLinks } = require('./index.js');
 
+// Declare options https://www.npmjs.com/package/commander
+program
+    .option('--validate', 'validate links')
+    .option('--stats', 'show statistics instead of list')
+program.parse(process.argv);
+const options = program.opts();
+// Get path argument
 const [, , ...args] = process.argv;
-mdLinks(`${args}`, { validate: true }).then((linksArray) => {
-    console.log(linksArray);
-});
+
+if (args.length !== 1) {
+    console.log("Please enter exactly one argument")
+    return;
+}
+
+// Options cases
+if (!options.validate && !options.stats) {
+    mdLinks(`${args[0]}`, { validate: false }).then((linksArray) => {
+        console.log(linksArray);
+    });
+} else if (options.validate && !options.stats) {
+    mdLinks(`${args[0]}`, { validate: true }).then((linksArray) => {
+        console.log(linksArray);
+    });
+} else if (options.stats && !options.validate) {
+    console.log("proximamente");
+} else if (options.stats && options.validate) {
+    console.log("proximamente");
+}
+
+
+
+
