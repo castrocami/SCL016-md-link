80f2bf92aea063687a947a36a472a57bf1f15970
Camila Castro <ccastromn@gmail.com>
2021-05-23 18:33:20 -0400

    Feat: Stats and stats+validate options

--- cli.js
+++ cli.js
@@ -11,7 +11,7 @@ const options = program.opts();
 // Get path argument
 const [, , ...args] = process.argv;
 
-if (args.length !== 1) {
+if (args.length !== 3) {
     console.log("Please enter exactly one argument")
     return;
 }
@@ -26,9 +26,40 @@ if (!options.validate && !options.stats) {
         console.log(linksArray);
     });
 } else if (options.stats && !options.validate) {
-    console.log("proximamente");
+    mdLinks(`${args[0]}`, { validate: true }).then((linksArray) => {
+        let totalLinks = 0;
+        let uniqueLinks = 0;
+        totalLinks = linksArray.length;
+        console.log(`Total: ${totalLinks}`);
+        const o = {};
+        linksArray.forEach(element => {
+            if (o[element.link] == null) {
+                uniqueLinks++;
+            };
+            o[element.link] = true;
+        });
+        console.log(`Unique: ${uniqueLinks}`);
+    });
 } else if (options.stats && options.validate) {
-    console.log("proximamente");
+    mdLinks(`${args[0]}`, { validate: true }).then((linksArray) => {
+        let totalLinks = 0;
+        let uniqueLinks = 0;
+        let brokenLinks = 0;
+        totalLinks = linksArray.length;
+        console.log(`Total: ${totalLinks}`);
+        const o = {};
+        linksArray.forEach(element => {
+            if (o[element.link] == null) {
+                uniqueLinks++;
+            };
+            o[element.link] = true;
+            if (element.ok.includes('fail')) {
+                brokenLinks++;
+            }
+        });
+        console.log(`Unique: ${uniqueLinks}`);
+        console.log(`Broken: ${brokenLinks}`)
+    });
 }
 
 
